4c9cdc64faed70445dd0a97e29bdcc166dd10b10
kaique <1247399@sga.pucminas.br>
2022-05-15 23:12:29 -0300

    Update project

--- src/SimpleSignProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SimpleSign.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
