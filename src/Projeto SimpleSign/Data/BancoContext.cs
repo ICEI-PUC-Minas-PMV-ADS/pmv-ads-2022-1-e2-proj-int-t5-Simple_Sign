c514baedb316bee5a0d2d9ac0eebf04a98c3492e
Brenandson <breno.adm15@gmail.com>
2022-05-19 00:29:41 -0300

    criação das entidades para upload de arquivos

--- src/Projeto SimpleSign/Data/BancoContext.cs
+++ src/Projeto SimpleSign/Data/BancoContext.cs
@@ -3,7 +3,7 @@ using SimpleSign.Models;
 
 namespace SimpleSign.Data
 {
-    public class BancoContent :DbContext 
+    public class BancoContent : DbContext 
     {
         public BancoContent(DbContextOptions<BancoContent> options)
             : base(options)
@@ -12,7 +12,7 @@ namespace SimpleSign.Data
         
         public DbSet<ContatoModel> Contatos { get; set; }
         public DbSet<UsuarioModel> Usuarios { get; set; }
-
+        public DbSet<ArquivoModel> Arquivos { get; set; }
 
     }
 }
---
+++ src/Projeto SimpleSign/Models/ArquivoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleSign.Models
+{
+    public class ArquivoModel
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public byte[] Dados { get; set; }
+        public string ContentType { get; set; }
+    }
+}
